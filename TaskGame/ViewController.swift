a20f1441ae298f784e470ad70577a2621a2e32e9
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-11-14 14:56:43 +0000

    Initial Commit

---
+++ TaskGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskGame
+//
+//  Created by Oleksandr Smakhtin on 14/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
